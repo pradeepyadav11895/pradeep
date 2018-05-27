cdd0efd62efb1c68d3aaabb2c4542e857c1e31ee
pradeepyadav11895 <er.pradeepy118@gmail.com>
2018-05-27 12:03:01 +0530

    artitecture coomit

---
+++ src/main/java/com/pradeep/PradeepApplication.java
@@ -0,0 +1,12 @@
+package com.pradeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PradeepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PradeepApplication.class, args);
+	}
+}
---
+++ src/test/java/com/pradeep/PradeepApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pradeep;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PradeepApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
