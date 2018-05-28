080d6ed6f9a2a19a657435fdd5b61613121f1d78
pradeepyadav11895 <er.pradeepy118@gmail.com>
2018-05-28 10:22:07 +0530

    i18n file commited

--- src/test/java/com/pradeep/PradeepApplicationTests.java
+++ src/test/java/com/pradeep/PradeepApplicationTests.java
@@ -1,16 +1,28 @@
 package com.pradeep;
 
+import com.pradeep.web.i18n.I18NService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@WebAppConfiguration
 public class PradeepApplicationTests {
 
+	@Autowired
+	private I18NService i18NService;
+
 	@Test
-	public void contextLoads() {
+	public void testMessageByLocaleService(){
+		String expectedResult="Bootstrap starter template";
+		String messageId="index.main.callout";
+		String actual=i18NService.getMessage(messageId);
+		Assert.assertEquals("The actual and Expected Strings not macth",expectedResult,actual);
 	}
 
 }
