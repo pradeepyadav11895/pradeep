fe16910ac905bae819b20a1cac943d4ffc5b6f68
pradeepyadav11895 <er.pradeepy118@gmail.com>
2018-06-11 16:28:21 +0530

    added login forma and login path in nav bar

---
+++ src/main/java/com/pradeep/web/controllers/LoginController.java
@@ -0,0 +1,14 @@
+package com.pradeep.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+    public static String LOGIN_VIEW_NAME="user/login";
+    @RequestMapping("/login")
+    public String login(){
+        return LOGIN_VIEW_NAME;
+    }
+}
