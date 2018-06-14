3aaef02f2903bbc867902cd71aaa6cf1b68ea0aa
pradeepyadav11895 <er.pradeepy118@gmail.com>
2018-06-14 15:09:35 +0530

    add login and logout using spring secquirity

---
+++ src/main/java/com/pradeep/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.pradeep.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * Created by tedonema on 26/03/2016.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /** Public URLs. */
+    private static final String[] PUBLIC_MATCHERS = {
+            "/webjars/**",
+            "/css/**",
+            "/js/**",
+            "/images/**",
+            "/",
+            "/about/**",
+            "/contact/**",
+            "/error/**/*",
+    };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers(PUBLIC_MATCHERS).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").defaultSuccessUrl("/payload")
+                .failureUrl("/login?error").permitAll()
+                .and()
+                .logout().permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("$2a$10$YDdBL8v9M3X4qIx3KMww7eF8dYzv18oPOThnrirKl6bdPsWztj6ru").roles("USER")
+                .and()
+                .passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+
+}
---
+++ src/main/java/com/pradeep/config/bcript.java
@@ -0,0 +1,10 @@
+package com.pradeep.config;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class bcript {
+
+    public static  void main(String args[]){
+        System.out.println("enc-- "+new BCryptPasswordEncoder().encode("password"));
+    }
+}
