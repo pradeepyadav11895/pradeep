1ed10481690ddf38219d5bd7594148ae01c0fa46
pradeepyadav11895 <er.pradeepy118@gmail.com>
2018-06-16 20:19:18 +0530

    created the jpa entity for spring Secqurity

---
+++ src/main/java/com/pradeep/backend/persistence/domain/backend/Role.java
@@ -0,0 +1,65 @@
+package com.pradeep.backend.persistence.domain.backend;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+public class Role  implements Serializable {
+    /** The Serial version UID for serializable classes */
+    private static final long serialVersionUID=1L;
+
+    @Id
+    private int id;
+
+    private String name;
+
+
+
+    @OneToMany(mappedBy = "role",cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    private Set<UserRole> userRoles= new HashSet<>();
+
+    public Role(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<UserRole> getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(Set<UserRole> userRoles) {
+        this.userRoles = userRoles;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return id == role.id;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id);
+    }
+}
